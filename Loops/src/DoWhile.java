57ea6557ba99e14f711d0656a71a9267d83aa03d
sm8du <sm8du@virginia.edu>
2016-07-12 01:56:01 -0400

    Initial

--- Loops/src/DoWhile.java
+++ Loops/src/DoWhile.java
@@ -1,10 +1,16 @@
 
 public class DoWhile {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("jsd");
-		l
-	}
+	 //The difference between do-while and while is that do-while evaluates 
+	 //its expression at the bottom of the loop instead of the top. Therefore, the statements within the do 
+	 //block are always executed at least once, as shown in the following DoWhileDemo program:
+	
+	public static void main(String[] args){
+	        int count = 12;
+	        do {
+	            System.out.println("Count is: " + count);
+	            count++;
+	        } while (count < 11);
+	    }
 
 }
