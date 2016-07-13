580e2bc8bc69a1d15924e64929651c6051d7d326
sm8du <sm8du@virginia.edu>
2016-07-13 09:52:02 -0400

    Maven

---
+++ TicTacToeApplication/src/TicTacToe.java
@@ -0,0 +1,14 @@
+
+	protected char[] board;
+	protected char userMarker;
+	protected char aiMarker;
+	protected char winner;
+	protected char currentMarker;
+
+
+public class TicTacToe {
+	
+	
+	
+
+}
